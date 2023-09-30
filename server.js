fea745d671d4b8282df3e63b16bd1a9c71d80a70
mariya-ivanova <maria_m11m@yahoo.com>
2023-09-30 20:22:15 +0300

    initial version

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname));
+app.use(express.static('public'));
+
+var port = 3000;
+app.listen(port, function() {
+  console.log('Server is running at http://localhost:' + port);
+});
